c17ac585f87261c3c33c0dfa7c127b686432aca1
Muhammad Ahmed Khan <makhan128@myseneca.ca>
2026-05-15 15:37:36 -0400

    Added Lab2e

---
+++ lab2e.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Author: Muhammad Ahmed Khan
+# Author ID: makhan128
+# Date Created: 2026/05/15
+timer = 10
+while timer != 0:
+    print(timer)
+    timer= timer - 1
+print('blast off!')
