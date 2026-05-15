25f5ec6c7e286728e2a8e253983b9056be57e3b2
Muhammad Ahmed Khan <makhan128@myseneca.ca>
2026-05-15 15:38:06 -0400

    Added Lab2f

---
+++ lab2f.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: Muhammad Ahmed Khan
+#Author ID: makhan128
+# Date Created: 2026/05/15
+import sys
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer= timer - 1
+print('blast off!')
