6a36d46a65c2bcf0c3bc2ce8e8baf5c6a3c659d2
Muhammad Ahmed Khan <makhan128@myseneca.ca>
2026-05-15 15:36:19 -0400

    Added Lab2b

---
+++ lab2b.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+name = input('Name: ')
+
+age = input('Age: ')
+# print('Name: ' + name)
+
+# print('Age: ' + age)
+print('Hi ' + name + ', you are '+ str(age) + ' years old.')
